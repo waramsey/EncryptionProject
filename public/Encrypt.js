59203781176f0841e6dee4eba043685a04a083f3
waramsey <46686986+waramsey@users.noreply.github.com>
2019-12-14 14:41:29 -0600

    First Commit
    I'm suprised I hadn't published this yet.  The project has nearly functional encryption capabilities and some of the framework for decryption in place.

--- public/Encrypt.js
+++ public/Encrypt.js
@@ -1,105 +1,161 @@
-import { createRequireFromPath } from "module";
-
 //Make Connection
 var socket = io.connect('http://localhost:4000');
 
 //Query DOM
 var message = document.getElementById('message');
 var submit = document.getElementById('encrypt');
+var postEncryption = document.getElementById('postEncryption');
+var cypherMessage = document.getElementById('cypher-message');
 
-//Prime Number Finder
-function primeTest(n, k) {
-	if (n % 2 === 0) {
-		return false;
-	}
-	
-	var d = 1;
-    var temp = n - 1;
-    var r = 0;
-    do {
-        temp = temp / 2;
-        r++;
-    } while (Number.isInteger(temp));
-    r--;
-    
-    while (n - 1 !== d * Math.pow(2, r)) {
-		d++;
-	}
-
-	for (; k >= 0; k--) {
-		if (!millerRabin(n, d)) {
-			return false;
-		}
-	}
-	return true;
-}
+//Keys
+var pubKey;
+var privKey;
 
 
-//Miller-Rabin test, which determines if a number is composit or not.
-function millerRabin(n, d) {
-	var a = Math.ceil((n - 3) * Math.random()) + 1;
-	var testRes = Math.pow(a, d) % n;
-	if (testRes === 1 || testRes === (n - 1)) {
-		return true;
-	}
-	
-	while (true) {
-		testRes = Math.pow(testRes, 2) % n;
-		if (testRes === 1) {
-			return false;
-		} else if (testRes === n - 1) {
-			return true;
-		}
-	}
-}
-
-//User input
-socket.on('encryptor-div', function(data){
-    
-    data.message.value = '';
+//listens for the user's click on the encrypt button
+submit.addEventListener('click', function(){
+    socket.emit('encryptor-div', {
+        message: message.value
+    });
 });
 
-//Greatest Common Denominator
-function gcd(a, b) {
-    if (b == 0) {
-        return a;
+//when user clicks on encrypt, encrypts the message
+socket.on('encryptor-div', function(data){
+    postEncryption.innerHTML += '<p>' + encryptInput(data.message) + '</p>';
+    cypherMessage.value = encryptInput(data.message);
+    //postEncryption.innerHTML += '<p>' + data.message + '</p>';
+});
+
+
+//Encrypt input (add generated key to plaintext characters, acceptable range is 32 - 126)
+//Dr Ricks thinks it is encrypted in chunks, continue research
+function encryptInput(userInput) {
+    genKey();
+
+    var publicKey = pubKey[0];
+    var publicExponent = pubKey[1];
+
+    var cypherText = '';
+
+    for (var i = 0; i < userInput.length; i++) {
+        //encrypt character
+        var m = Math.pow(userInput.charCodeAt(i), publicExponent);
+
+        m %= publicKey;
+        cypherText += m.toString();
     }
 
-    return gcd(b, a % b);
+    var m = Math.pow(userInput, publicExponent);
+    m %= publicKey;
+    cypherText += m.toString();
+
+    return cypherText;
 }
 
-//Generate Key
-function genKey() {
-    var a = prime();
-    var b = prime();
 
-    var n = a * b;
+//Generate Key: a public key is made up of 
+//"n" (two prime numbers, 1024 digits each) 
+//and "exp" (the public exponent)
+function genKey() {
+    console.log('randlargeprime a');
+    var a = randLargePrime();
+    console.log('randlargeprime b');
+    var b = randLargePrime();
+
+    var n = a * b; //1st part of both keys
 
     var phi = (a - 1) * (b - 1);
-    var exp = 2; //starting at 1 could make it easy to crack
+    var exp = 2; //2nd part of PUBLIC key
     while (exp < phi) {
         if(gcd(exp, phi) == 1) {
             break;
         }
         exp++;
     }
+
+    //phi is already phi of n
+    //(d * exp) % phi = 1
+    var d = 0;
+    do {
+        d++;
+    } while ((d * exp) % phi !== 1);
+
+    pubKey = [n, exp];
+    privKey = [n, d];
 }
 
-//Encrypt input (add generated key to plaintext characters, acceptable range is 32 - 126)
-function encryptInput(plainChar, publicExponent, publicKey) {
-    var m = plainChar;
-    for(var i = 0; i < publicExponent; i++) {
-        m *= plainChar;
-    }
 
-    m %= publicKey;
+//generates a prime 1024 digits long.  Had to reduce the size of the prime so javascript wouldn't call it 'infinity.'
+function randLargePrime() {
+    var guess = 0;
+    var value = 0;
+    console.log('entered randlargeprime loop');
 
-    while (m < 32) {
-        m += 94;
-    }
-    while (m > 126) {
-        m -= 94;
-    }
+    do {
+        guess = Math.pow(10, 1) + Math.floor(Math.random() * 9 * Math.pow(10, 1));
+    } while (!primeTest(guess, 5)); //the '20' is how many times we want to run the miller-rabin test.
+    console.log('escaped randlargeprime loop');
 
-    return m;
+    return value;
 }
+
+
+//Prime Number Finder
+function primeTest(n, k) {
+	if (n % 2 === 0) {
+		return false;
+	}
+    console.log('primetest 1');
+
+    //We use the Fermat Primality Test to determine primality
+    for (; k >= 0; k--) {
+        var a = k;
+    
+        if (gcd(a, n) !== 1) {
+            return false;
+        } else {
+            var power = a;
+            for (var i = n - 1; i > 0; i--) {
+                power *= a;
+            }
+            if (power % n !== 1) {
+                return false;
+            }
+        }
+    }
+
+	return true;
+}
+
+
+//Greatest Common Denominator
+function gcd(a, b) {
+    if (b == 0) {
+        return a;
+    }
+    console.log('found gcd');
+
+    return gcd(b, a % b);
+}
+
+
+//Miller-Rabin test, which determines if a number is composit or not.
+// function millerRabin(n, d) {
+//     console.log('entered miller rabin');
+
+// 	var a = Math.ceil((n - 3) * Math.random()) + 1;
+// 	var testRes = Math.pow(a, d) % n; //This number is too darn big
+// 	if (testRes === 1 || testRes === (n - 1)) {
+// 		return true;
+// 	}
+//     console.log('miller rabin pt 2');
+
+// 	while (true) {
+// 		testRes = Math.pow(testRes, 2) % n;
+// 		if (testRes === 1) {
+// 			return false;
+// 		} else if (testRes === n - 1) {
+// 			return true;
+// 		}
+// 	}
+// }
